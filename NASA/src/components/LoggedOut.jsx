5fe545e4eca5df7856e6c3c2ea56fc7d08968f3a
nmontiel42 <montielarce9@gmail.com>
2024-10-03 19:11:08 +0200

    commit

--- NASA/src/components/LoggedOut.jsx
+++ NASA/src/components/LoggedOut.jsx
@@ -4,24 +4,35 @@ export default function LoggedOut() {
   const { login, register } = useKindeAuth();
   return (
     <>
-    <body className="bg-customColor1">
+    <body className="h-screen bg-gradient-to-bl from-green5 via-blue1 to-purple4">
         <header>
           <nav className="bg-black bg-opacity-50 flex justify-between items-center p-4">
-            <h1 className="font-bold text-2xl">BioInvaders</h1>
-            <div className="flex gap-2">
-              <button className="rounded-md p-1" onClick={login}>
+            <h1 className="font-bold text-2xl text-white">BioInvaders</h1>
+            <img className="w-20 h-full" src="public/Space_Apps_Default_Logo_-_2-Col.width-440.jpegquality-60.png" />
+            <div className="flex gap-5 text-xl">
+              <button className="rounded-md p-1 h-full hover:underline text-white" 
+                onClick={login}>
                 Sign in
               </button>
-              <button className="bg-customColor5 text-white rounded-2xl p-2" onClick={register}>
+              <button className="rounded-md p-1 h-full bg-blue4 shadow-customHover hover:shadow-blue4" onClick={register}>
                 Sign up
               </button>
             </div>
           </nav>
         </header>
+
         <br />
+
         <main>
-          <h1 className="flex justify-center">Welcome to our page!</h1>
+          <h1 className="text-white flex justify-center">Welcome to our page!</h1>
+          <div className="flex justify-center">
+            <img src="public/logo.png" />
+          </div>
         </main>
+
+        <footer className="text-white fixed bottom-0 left-0 right-0 flex justify-center p-4">
+          <p>© 2024 BioInvaders - All rights reserved.</p>
+        </footer>
       </body>
     </>
   );
--- NASA/tailwind.config.js
+++ NASA/tailwind.config.js
@@ -5,14 +5,51 @@ export const content = [
 
 export const theme = {
   extend: {
-    colors: {
-      customColor1: '#cbe58e',
-      customColor2: '#013026', 
-      customColor3: '#107e57',
-      customColor4: '#a1ce3f',
-      customColor5: '#014760'
+    backgroundImage: {
+      'space': "url('public/5a382399ba548cccde42b698be37a4c0.jpg')"
     },
+    colors: {
+
+      /* Verdes */
+
+      green1: '#cbe58e',
+      green2: '#013026', 
+      green3: '#107e57',
+      green4: '#a1ce3f',
+      green5: '#1d8348',
+      green6: '#52be80',
+      green7: '#82e0aa',
+      green8: '#a9dfbf',
+
+      /* Azules */
+
+      blue1: '#014760',
+      blue2: '#1a3a5f',
+      blue3: '#1b4f72',
+      blue4: '#5dade2',
+      blue5: '#85c1e9',
+
+      /* Morados */
+
+      purple1: '#9B30FF',
+      purple2: '#6A0DAD',
+      purple3: '#5D3FD3',
+      purple4: '#4B0082',
+      purple5: '#8A2BE2',
+      purple6: '#301934'
+    },
+
+    boxShadow: {
+      customColor3: '0 0 5px customColor3, 0 0 25px customColor3',
+      customHover: '0 0 5px customColor3, 0 0 25px customColor3, 0 0 50px customColor3, 0 0 100px customColor3, 0 0 200px customColor3',
+    },
+
+    textShadow: {
+      custom: '0 0 5px rgba(255, 255, 255, 0.8), 0 0 10px rgba(255, 255, 255, 0.5)'
+    }
   },
 };
 
-export const plugins = [];
+export const plugins = [
+  require('tailwindcss-textshadow'),
+];
